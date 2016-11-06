323e91017e807d4a4d6f20eb4ae60334811432bf
Aniket Rao <aniket.rao92@gmail.com>
2016-11-05 21:13:38 -0700

    Models, URLs and databases

---
+++ music/models.py
@@ -0,0 +1,20 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+# basically a blueprint of your app. This is linked to your database.
+# so every variable in a class is a column in your database.
+class Album(models.Model):
+    artist = models.CharField(max_length=250)
+    name = models.CharField(max_length=500)
+    genre = models.CharField(max_length=100)
+    album_logo = models.CharField(max_length=150)
+
+    def __str__(self):
+    	return self.name + ', ' + self.artist
+
+class Song(models.Model):
+ 	album_name = models.ForeignKey(Album,on_delete=models.CASCADE)
+ 	file_type = models.CharField(max_length=100)
+ 	song_title = models.CharField(max_length=250)
