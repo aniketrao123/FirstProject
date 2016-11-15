3c3f5a02b91c9c0ff4afc03149ef22f8abba2ab9
Aniket Rao <aniket.rao92@gmail.com>
2016-11-14 20:46:55 -0800

    Rendering a template

--- music/views.py
+++ music/views.py
@@ -1,4 +1,11 @@
 from django.http import HttpResponse
+from music.models import Album
+from django.shortcuts import render
 
 def index(request):
-	return HttpResponse('<h1> Hello ! This is the music homepage!</h1>')
+	all_albums = Album.objects.all()
+	context = {'all_albums' : all_albums}
+	return render(request, 'music/index.html', context)
+
+def details(request,album_id):
+	return HttpResponse('<h1> The album id is: ' + str(album_id) + '</h1>')
