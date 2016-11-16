79f9fdd15b17934eec6e13b29362813467ea3da1
Aniket Rao <aniket.rao92@gmail.com>
2016-11-15 16:12:04 -0800

    used get_object_or_404 instead of try/except

--- music/views.py
+++ music/views.py
@@ -1,11 +1,13 @@
-from django.http import HttpResponse
+from django.http import Http404
 from music.models import Album
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 
 def index(request):
 	all_albums = Album.objects.all()
-	context = {'all_albums' : all_albums}
+	context = {'all_albums':all_albums}
 	return render(request, 'music/index.html', context)
 
 def details(request,album_id):
-	return HttpResponse('<h1> The album id is: ' + str(album_id) + '</h1>')
+	album = get_object_or_404(Album,id=album_id)
+	context = {'album':album}
+	return render(request, 'music/detail.html', context)
