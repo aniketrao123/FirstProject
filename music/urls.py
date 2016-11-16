59d050b053ffe6940583c3930ce8ba0796b996f7
Aniket Rao <aniket.rao92@gmail.com>
2016-11-15 16:11:40 -0800

    specified app name as music

--- music/urls.py
+++ music/urls.py
@@ -1,11 +1,10 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'music'
 urlpatterns = [
-     
     #/music/
     url(r'^$', views.index,name='index'),
-
     #/music/234/
     url(r'^(?P<album_id>[0-9]+)/$',views.details,name='details')   
 ]
